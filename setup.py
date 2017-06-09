98a85036b8006c1f8cb512ea8901f32c6bef4815
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-06-09 11:13:13 +0100

    Initialize package and add MIT license

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tphysics',
+      version='0.1',
+      description='Package to create a basic physics engine for turtle',
+      url='http://github.com/thebillington/tphysics',
+      author='thebillington',
+      author_email='billy.rebecchi@googlemail.com',
+      license='MIT',
+      packages=['tphysics'],
+      zip_safe=False)
