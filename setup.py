6d0878931687e5e0e5b5cb8aa73f0dcb9ed1b826
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-06-09 11:16:50 +0100

    Maintain version history

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tphysics',
-      version='0.1',
+      version='0.2',
       description='Package to create a basic physics engine for turtle',
       url='http://github.com/thebillington/tphysics',
       author='thebillington',
