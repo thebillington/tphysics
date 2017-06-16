2d6862d84cb593253d1e3d73473a83d76f9a04e9
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-06-16 11:20:25 +0100

    Update bouncy ball example to use onclick.

--- Examples/BouncyBall/BouncyBall.py
+++ Examples/BouncyBall/BouncyBall.py
@@ -1,5 +1,5 @@
 #Imports
-from tphysics import VerletCircle, Rectangle, Game
+from tphysics import VerletCircle, Rectangle, Point, Game
 from Tkinter import TclError
 
 #Create the game
@@ -13,37 +13,69 @@ for w in walls:
 	g.add_shape(w)
 	
 #Create a bouncy ball
-ball = VerletCircle(0, 0, 10, 10, 50)
+ball = VerletCircle(0, 0, 50, 10, 5)
 g.add_shape(ball)
 
 #Set the physics constants
-elasticity = 1
-friction = 0
-gravity = 0.2
+elasticity = 0.8
+friction = 0.2
+gravity = 0.6
 max_fall = 10
+
+#Create a boolean to store whether the ball was clicked
+global clicked
+clicked = False
+
+#Create a boolean to store whether the game is running
+global running
+running = True
+
+#Functions to handle mouse clicks
+def click(x, y):
 	
+	#Get the globals
+	global clicked
+	global running
+	#Check whether the click collides with the ball
+	if ball.collide(Point(x,y)) and not clicked:
+		#Set clicked to true
+		clicked = True
+		running = False
+	elif clicked:
+		#Set the current position of the ball to the mouse x and y
+		ball.x = x
+		ball.y = y
+		clicked = False
+		running = True
+		
+#Add click listeners to the game
+g.addclick(click)
+
 #Game loop
 while True:
 	
-	#Update the ball
-	ball.update()
+	#If the game is running
+	if running:
 	
-	#Implement gravity
-	if ball.getyspeed() > -max_fall:
-		#Change the y speed
-		ball.setyspeed(ball.getyspeed() - gravity)
-	
-	#If the ball hits a side wall
-	for i in range(2):
-		if ball.collide(walls[i]) != 0:
-			#Bounce on the x
-			ball.bouncex(elasticity)
-	
-	#If the ball hits a vertical wall
-	for i in range(2,4):
-		if ball.collide(walls[i]):
-			#Bounce on the y
-			ball.bouncey(elasticity, friction)
+		#Update the ball
+		ball.update()
+		
+		#Implement gravity
+		if ball.getyspeed() > -max_fall:
+			#Change the y speed
+			ball.setyspeed(ball.getyspeed() - gravity)
+		
+		#If the ball hits a side wall
+		for i in range(2):
+			if ball.collide(walls[i]) != 0:
+				#Bounce on the x
+				ball.bouncex(elasticity)
+		
+		#If the ball hits a vertical wall
+		for i in range(2,4):
+			if ball.collide(walls[i]):
+				#Bounce on the y
+				ball.bouncey(elasticity, friction)
 	
 	#If the window hasn't been closed update the game
 	try:
