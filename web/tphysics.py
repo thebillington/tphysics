a146ae1092489e3b2b8f8b2ca55aaacf33d23227
Billy Rebecchi <billy.rebecchi@googlemail.com>
2026-06-07 10:31:41 +0100

    Remove sprite functionality and example
    - Remove Sprite class from tphysics.py
    - Remove self.sprites list from Game.__init__
    - Remove Sprite Demo from example dropdown
    - Delete examples/sprites/ directory

--- web/tphysics.py
+++ web/tphysics.py
@@ -208,9 +208,6 @@ class Game:
         #Create a key listener
         self.keylistener = KeyListener(self.window)
         
-        #Create an empty list of sprites
-        self.sprites = []
-
         # Set the sleep time
         self.sleep = sleep_time
 
@@ -605,54 +602,4 @@ class Button:
         # Return the point to rectangle collision check using the specified point
         return self.rect.collide(Point(x,y))
 
-# SPRITES
 
-#Create an object to hold a sprite
-class Sprite:
-    
-    #Constructor
-    def __init__(self, img, window, x, y):
-        
-        #Store the image resource
-        self.img = img
-        
-        #Store the window
-        self.window = window
-        
-        #Add the image to the window
-        window.addshape(self.img)
-        
-        #Create a turtle object
-        self.turtle = Turtle()
-        self.turtle.penup()
-        
-        #Set the image for the turtle
-        self.turtle.shape(self.img)
-        
-        #Set the x and y position of the turtle
-        self.x = x
-        self.y = y
-        
-        #Set the turtle ot visible by default
-        self.visible = True
-        
-        #Set the initial position
-        self.move(0, 0)
-        
-    #Function to move the turtle
-    def move(self, x, y):
-        
-        #Change the values of the position
-        self.x += x
-        self.y += y
-        
-        #Move the sprite
-        self.turtle.goto(self.x, self.y)
-        
-    #Functions to set the sprite visible or invisible
-    def setvisibile(self):
-        self.visible = True
-        self.turtle.showturtle()
-    def setinvisible(self):
-        self.visible = False
-        self.turtle.hideturtle()
